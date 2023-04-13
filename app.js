e8089c296c2e332ca920cc08acfcad51fe7aee75
Muhammad Hamza <hs8403340@gmail.com>
2023-04-13 13:58:08 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,74 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+mongoose.set("strictQuery", false);
+main().catch((err) => console.log(err));
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const User = new mongoose.model("User", userSchema);
+
+app.post("/register", function (req, res) {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  );
+});
+
+app.post("/login", function (req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  req.login(user, function (err) {
+    if (err) {
+      res.redirect("/home");
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.listen(3000, () => {
+  console.log("Server is set up at port 3000");
+});
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
